c12b8727104d49376afa5588f330dfb68b91892a
Chris Parnin <chris.parnin@gmail.com>
2016-03-04 23:15:21 -0500

    boom: full REST

---
+++ src/main/java/servlets/JerseyREST.java
@@ -0,0 +1,18 @@
+package main.java.servlets;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+ 
+@Path("/users")
+public class JerseyREST 
+{
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+		String output = "Jersey say : " + msg;
+		return Response.status(200).entity(output).build();
+ 	}
+
+}
