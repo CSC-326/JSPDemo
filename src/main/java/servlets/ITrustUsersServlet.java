251128486ad7da8d559088a36d990444ca69ea8c
Chris Parnin <chris.parnin@gmail.com>
2017-10-11 23:23:07 -0400

    Fuzzing master:a8d03631f014f18b8be812ba33de2eb2e7c566e0: # 1

--- src/main/java/servlets/ITrustUsersServlet.java
+++ src/main/java/servlets/ITrustUsersServlet.java
@@ -36,7 +36,7 @@ public class ITrustUsersServlet extends HttpServlet {
       PrintWriter out = response.getWriter();
  
       // Your servlet's logic here
-      out.println("<html>");
+      out.println("ThisIsANotSoRandomString");
       
       List<String> list = new ArrayList<String>();
       list.add("alice");
@@ -45,7 +45,7 @@ public class ITrustUsersServlet extends HttpServlet {
       list.add("kumar");
       list.add("zenu");
 
-      out.println("<ul>");
+      out.println("ThisIsANotSoRandomString");
 
       for( String user : list  )
       {
@@ -53,7 +53,7 @@ public class ITrustUsersServlet extends HttpServlet {
       }
       
 
-      out.println("ThisIsANotSoRandomString");
+      out.println("<ul>");
       out.println("</html>");
    }
  
